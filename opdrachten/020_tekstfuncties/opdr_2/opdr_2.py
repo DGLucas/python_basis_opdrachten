7d9bea6f65f18987ff498400154312584d7d2b7e
Lucas <lucashattem@gmail.com>
2023-09-28 12:15:26 +0200

    020 opdr 1,2,3

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -4,6 +4,8 @@
 
 # Hier komt je code...
 
-my_list = ...
+my_list = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
+tel_t = my_list.lower().count("t") # Hier wordt de string eerst omgezet naar kleine letters om zowel "T" als "t" te tellen
+print(f"De letter 't' komt {tel_t} keer voor in de tekst.") #De "f" voor de print-functie in Python staat voor "formatted string". Dit wordt ook wel een f-string genoemd. F-strings zijn een handige manier om tekst samen te stellen waarin variabelen of expressies worden ingevoegd.
 
 print()
--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -3,3 +3,44 @@
 # Groep:
 
 # Hier komt je code...
+
+aantal_kerstbomen = 5 #Het aantal bomen die geprint moeten worden
+
+for i in range(aantal_kerstbomen):
+    print("     *      ", end=" ") 
+print("")
+
+for i in range(aantal_kerstbomen):
+    print("    ***     ", end=" ")
+print("")
+
+for i in range(aantal_kerstbomen):
+    print("   *****    ", end=" ")
+print("")
+
+for i in range(aantal_kerstbomen):
+    print("  *******   ", end=" ")
+print("")
+
+for i in range(aantal_kerstbomen):
+    print(" *********  ", end=" ")
+print("")
+
+for i in range(aantal_kerstbomen):
+    print("    ***     ", end=" ")
+print("")
+
+for i in range(aantal_kerstbomen):
+    print("    ***     ", end=" ")
+print("")
+
+for i in range(aantal_kerstbomen):
+    print("    ***     ", end=" ")
+print("")
+
+
+
+
+
+
+
